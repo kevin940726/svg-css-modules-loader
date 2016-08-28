f2668f52a215f9194434e537b67e5317c96365fc
kevin940726 <kevin830726@gmail.com>
2016-08-28 11:05:46 +0800

    callback to async/await

--- test/test.js
+++ test/test.js
@@ -10,20 +10,21 @@ const generate = genericNames('[name]__[local]___[hash:base64:5]', {
 
 const PATH = path.join(__dirname, 'test.svg')
 
-const callTest = (t, source, expected) => {
-  const module = {
-    resourcePath: PATH,
-    async: () => (err, result) => {
-      if (err) {
-        throw err
+const transform = source => {
+  return new Promise((resolve, reject) => {
+    const module = {
+      resourcePath: PATH,
+      async: () => (err, result) => {
+        if (err) {
+          reject(err)
+        }
+
+        resolve(result)
       }
-
-      t.is(result, expected)
-      t.end()
     }
-  }
 
-  svgCSSModules.call(module, source)
+    svgCSSModules.call(module, source)
+  })
 }
 
 test('it should correctly transform source to css-modules', t => {
@@ -32,40 +33,45 @@ test('it should correctly transform source to css-modules', t => {
   t.truthy(expected)
 })
 
-test.cb('it should handle camelCase attributes and tags in xmlMode', t => {
+test('it should handle camelCase attributes and tags in xmlMode', async t => {
   const source = '<svg viewBox="0 0 100 100"><clipPath></clipPath></svg>'
   const expected = '<svg viewBox="0 0 100 100"><clipPath/></svg>'
 
-  callTest(t, source, expected)
+  const result = await transform(source)
+  t.is(result, expected)
 })
 
-test.cb('it should transform id to css-modules', t => {
+test('it should transform id to css-modules', async t => {
   const id = 'a'
   const expectedId = generate(id, PATH)
   const source = '<svg><defs><style>#a{fill:none;}</style></defs><path id="a"/></svg>'
   const expected = `<svg><defs><style>#${expectedId}{fill:none;}</style></defs><path id="${expectedId}"/></svg>`
 
-  callTest(t, source, expected)
+  const result = await transform(source)
+  t.is(result, expected)
 })
 
 {
-  const macro = callTest
+  const macro = (t, input, expect) => t.is(input, expect)
   macro.title = (providedTitle, input) => `it should also transform id in url to css-modules with(out) quotes ${input}`
   const id = 'a'
   const expectedId = generate(id, PATH)
   const quotes = ['', '\'', '"']
-  quotes.forEach(quote => {
+  quotes.forEach(async quote => {
     const source = `<svg><defs><style>path{clip-path:url(${quote}#${id}${quote});}</style></defs><path/></svg>`
     const expected = `<svg><defs><style>path{clip-path:url(${quote}#${expectedId}${quote});}</style></defs><path/></svg>`
-    test.cb(macro, source, expected)
+
+    const result = await transform(source)
+    test(macro, result, expected)
   })
 }
 
-test.cb('it should transform id in "xlink:href"', t => {
+test.failing('it should transform id in "xlink:href"', async t => {
   const id = 'a'
   const expectedId = generate(id, PATH)
   const source = `<svg><g id="${id}"><use xlink:href="#${id}"/></g></svg>`
   const expected = `<svg><g id="${expectedId}"><use xlink:href="#${expectedId}"/></g></svg>`
 
-  callTest(t, source, expected)
+  const result = await transform(source)
+  t.is(result, expected)
 })
